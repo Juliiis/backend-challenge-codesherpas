7c4166dd3a72db362e90792da3b37d3f17193005
Juliiis <jumartinnob@gmail.com>
2022-04-17 09:58:15 +0200

    function express.json added  and endpoint post created

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express"
 
 const express = require('express')
 const app = express()
+app.use(express.json());
+
 
 app.get('/customer/:id', (req: Request, res: Response) => {
   const id = Number(req.params.id)
@@ -11,6 +13,12 @@ app.get('/customer/:id', (req: Request, res: Response) => {
 
   return res.json(customer)
 })
+
+app.post('/customer', (req: Request, res: Response) => {
+  const loQueMeManda = req.body
+  return res.json(loQueMeManda);
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
