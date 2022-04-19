c4e2f301cf1a711ad5bc9795d01321e2d9cff685
Juliiis <jumartinnob@gmail.com>
2022-04-19 17:12:07 +0200

    status messages created for each endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,29 @@ app.post('/customer', (req: Request, res: Response) => {
   const birthdate = req.body.birthdate
   
   const newCustomer = CustomerService.new(name, surname, email, birthdate)
-  return res.json(newCustomer);
+  if(newCustomer){
+    return res.status(200).send(newCustomer);
+  } 
+  return res.status(404).send('Failed creation. Please check the information to send and try again'); 
 })
 
 app.get('/customer/:id', (req: Request, res: Response) => {
   const id = Number(req.params.id)
 
   const customer = CustomerService.get(id)
-
-  return res.json(customer)
+  if(customer){
+    return res.status(200).send(customer);
+  } 
+  return res.status(404).send('The customer does not exist. Please try again'); 
 })
 
 app.get('/customers', (req:Request, res: Response) => {
   const customers = CustomerService.getAll()
 
-  return res.json(customers);
+  if(customers){
+    return res.status(200).send(customers);
+  } 
+  return res.status(404).send('The customers does not exist'); 
 })
 
 app.put('/customer/:id', (req:Request, res:Response) => {
@@ -37,7 +45,10 @@ app.put('/customer/:id', (req:Request, res:Response) => {
   const birthdate = req.body.birthdate
 
   const customerUpdated = CustomerService.updateCustomer(id, name, surname, email, birthdate)
-  return res.json(customerUpdated); 
+  if(customerUpdated){
+    return res.status(200).send(customerUpdated);
+  } 
+  return res.status(404).send('Update failed. The customer does not exist'); 
 })
 
 app.delete('/customer/:id', (req: Request, res: Response) => {
@@ -45,7 +56,10 @@ app.delete('/customer/:id', (req: Request, res: Response) => {
 
   const customer = CustomerService.delete(id)
 
-  return res.json(customer)
+  if(customer){
+    return res.status(200).send(customer);
+  } 
+  return res.status(404).send('Update failed. The customer does not exist');
 })
 
 
