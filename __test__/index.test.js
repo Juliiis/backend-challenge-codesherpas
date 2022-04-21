9f2fd0c7e2b22b919bd85817368356493d9b29ea
Juliiis <jumartinnob@gmail.com>
2022-04-21 23:31:33 +0200

    test for POST customer done

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,14 +1,34 @@
 const request = require('supertest');
-const app = require('../src/index');
+
+const app = require('../src/routes/index');
 
 describe('index.ts', () => {
+
+    const user = {
+        name: 'new user',
+        surname:'surname new user',
+        email: 'newuser@gmail.com',
+        birthdate: '03/09/1990'
+    }
+
+    describe('POST /customer', () => {
+        it('should post a new user', async () => {
+            const response = await request(app).post('/customer').send(user)
+
+            expect(response.error).toBe(false)
+            expect(response.status).toBe(200)
+            expect(response.body).toMatchObject(user)
+        })
+    })
+
     describe('GET /customers', () => {
         it('should return all users', async () => {
             const response = await request(app).get('/customers')
         
             expect(response.error).toBe(false)
             expect(response.status).toBe(200)
-            expect(response.body).toStrictEqual([])
+            expect(response.body.length).toBe(1)
+            expect(response.body[0]).toMatchObject(user)
         })
     })
 })
